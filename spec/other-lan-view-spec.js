d38b50c7bc527eb0830eb15428e2e992b752b8c7
nathanfreeman1111 <nathan.freeman9401@gmail.com>
2022-11-05 11:06:17 +0530

    first commit

---
+++ spec/other-lan-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import OtherLanView from '../lib/other-lan-view';
+
+describe('OtherLanView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
